243e36e268d9ee104dcd0855d3297c66346b42c0
Baptiste JULLION <contact@baptiste-jullion.dev>
2025-03-04 00:28:30 +0100

    feat: autogenerate id on creation

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -2,9 +2,11 @@
 
 namespace App\Entity;
 
+
 use App\Repository\ArticleRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Ramsey\Uuid\Uuid;
 use Ramsey\Uuid\UuidInterface;
 
 
@@ -14,18 +16,21 @@ class Article
     #[ORM\Id]
     #[ORM\Column(type: "uuid", unique: true)]
     private ?UuidInterface $id = null;
-
     #[ORM\Column(length: 255)]
     private ?string $slug = null;
-
     #[ORM\Column(length: 255)]
     private ?string $title = null;
-
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
     private ?\DateTimeInterface $published_at = null;
-
     #[ORM\Column(type: Types::TEXT)]
     private ?string $content = null;
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $cover = null;
+
+    public function __construct()
+    {
+        $this->id = Uuid::uuid4();
+    }
 
     public function getId(): ?UuidInterface
     {
@@ -76,6 +81,18 @@ class Article
     public function setContent(string $content): static
     {
         $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCover(): ?string
+    {
+        return $this->cover;
+    }
+
+    public function setCover(string|null $cover): static
+    {
+        $this->cover = $cover;
 
         return $this;
     }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,8 +2,10 @@
 
 namespace App\Entity;
 
+
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Ramsey\Uuid\Uuid;
 use Ramsey\Uuid\UuidInterface;
 
 
@@ -14,12 +16,15 @@ class User
     #[ORM\Id]
     #[ORM\Column(type: "uuid", unique: true)]
     private ?UuidInterface $id = null;
-
     #[ORM\Column(length: 255)]
     private ?string $email = null;
-
     #[ORM\Column(length: 255)]
     private ?string $password = null;
+
+    public function __construct()
+    {
+        $this->id = Uuid::uuid4();
+    }
 
     public function getId(): ?UuidInterface
     {
